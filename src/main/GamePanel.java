f646017f634932c8b4c782009a0f4bcad96e3879
Ralph Tan <ralphfedericktan@gmail.com>
2025-05-17 20:04:23 -0500

    Add Game Loop

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -3,12 +3,52 @@ package main;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable{
     public static final int WIDTH = 800;
     public static final int HEIGHT = 600;
+    final int FPS = 60;
+    Thread gameThread;
 
+    // GamePanel Constructor
     public GamePanel() {
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
         setBackground(Color.GRAY);
+    }
+
+    private void update() {
+
+    }
+
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+    }
+
+    @Override
+    public void run() {
+        // Game Loop - processes run continuously while game is running
+        double drawInterval = (double) 1000000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime)/drawInterval;
+            lastTime = currentTime;
+
+            // Executed every 1/60 of a second
+            if(delta >= 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+    }
+
+    // Instantiate Thread
+    public void launchGame() {
+        gameThread = new Thread(this);
+        gameThread.start(); // Calls run()
     }
 }
